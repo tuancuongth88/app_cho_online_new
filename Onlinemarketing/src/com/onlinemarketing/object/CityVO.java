f83e6af65e4732e45857e071f32c8d5f13323da5
tuancuongth88 <tuancuongth88@gmail.com>
2016-04-06 13:25:13 +0700

    fix city on post product

---
+++ Onlinemarketing/src/com/onlinemarketing/object/CityVO.java
@@ -0,0 +1,19 @@
+package com.onlinemarketing.object;
+
+public class CityVO {
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
